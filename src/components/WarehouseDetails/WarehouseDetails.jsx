2016e432dc2db8c73a439802e14712896f0036cb
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-13 11:15:45 -0800

    create skeleton for warehouse details component

---
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function WarehouseDetails() {
+  return (
+    <section className='warehouse'>
+        <article className="card-address">
+        <h4 className="title-address">Warehouse Address:</h4>
+        <p className="text-address"></p>
+        </article>
+
+        <article className="card-name">
+        <h4 className="title-name">Contact Name:</h4>
+        <p className="text-address"></p>
+        </article>
+
+        <article className="card-info">
+        <h4 className="title-info">Contact Information:</h4>
+        <p className="text-info"></p>
+        </article>
+
+    </section>
+  )
+}
+
+export default WarehouseDetails
--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails'
 
 export default function WarehousePage() {
   return (
-    <div></div>
+    <div>
+      <WarehouseDetails />
+    </div>
   )
 }
