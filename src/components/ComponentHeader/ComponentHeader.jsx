bf4d81249416e04621569d2f8373def243693a66
Renu Bhargavi Seetharaman <renubhargavi17@gmail.com>
2024-11-14 00:32:49 -0500

    edit warehouse component flex styling

--- src/components/ComponentHeader/ComponentHeader.jsx
+++ src/components/ComponentHeader/ComponentHeader.jsx
@@ -2,29 +2,31 @@ import React from 'react';
 import "../ComponentHeader/ComponentHeader.scss";
 import PropTypes from 'prop-types';
 
-export default function ComponentHeader({ text, backIcon, editIcon, editIconTablet }) {
+export default function ComponentHeader({ text, backIcon, editIcon, editIconTablet}) {
     return (
         <div className='componentHeader'>
             <div className='componentHeader-1'>
-                {backIcon && <img className='backIcon' src={backIcon} alt='Back Arrow Icon' />}
-                <h2 >{text}</h2>
+                {backIcon && (
+                    <button className='backIcon' >
+                        <img src={backIcon} alt='Back Arrow Icon' />
+                    </button>
+                )}
+                <h2 className='componentTitle'>{text}</h2>
             </div>
-
 
             {(editIcon || editIconTablet) && (
                 <div className='componentHeader-2'>
                     {editIcon && (
-                        <div className='editMobile'>
+                        <button className='editMobile' >
                             <img src={editIcon} className='editIcon' alt='Edit Icon for Mobile' />
-                        </div>
+                        </button>
                     )}
                     {editIconTablet && (
-                        <div className='editTablet'>
+                        <button className='editTablet' >
                             <img src={editIconTablet} className='editIconTablet' alt='Edit Icon for Tablet' />
                             <span className='editText'>Edit</span>
-                        </div>
+                        </button>
                     )}
-
                 </div>
             )}
         </div>
@@ -36,10 +38,12 @@ ComponentHeader.propTypes = {
     backIcon: PropTypes.string,
     editIcon: PropTypes.string,
     editIconTablet: PropTypes.string,
+    
 };
 
 ComponentHeader.defaultProps = {
     backIcon: null,
     editIcon: null,
     editIconTablet: null,
+    
 };
