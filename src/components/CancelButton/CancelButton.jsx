179f526d00b6a424d3efebe054b11449b3fb128e
Renu Bhargavi Seetharaman <renubhargavi17@gmail.com>
2024-11-14 11:19:08 -0500

    reusable components header, footer and edit warehouse

--- src/components/CancelButton/CancelButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import "../CancelButton/CancelButton.scss"
-export default function CancelButton() {
-  return (
-    <button type="button" className="cancelButton">Cancel</button>
-  )
-}
