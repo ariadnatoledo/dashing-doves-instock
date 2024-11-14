a8578e231c490a884d61ac3c55ea8691a3988881
Tanya Vezyr <tetiana.vezyr@gmail.com>
2024-11-14 12:50:28 -0700

    changes: q

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "../src/components/Footer/Footer";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import ListItems from "./components/ListItems/ListItems";
 import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+import EditWarehouse from "./components/EditWarehouse/EditWarehouse";
 
 
 
@@ -17,12 +18,10 @@ function App() {
       <div className="main__card">
         <Routes>
           <Route path="/" element={<ListItems items="warehouses" />} />
-          <Route
-            path="/warehouses"
-            element={<ListItems items="warehouses" />}
-          />
-          <Route path="/inventory" element={<ListItems items="inventory" />} />
+          <Route path="/warehouses" element={<ListItems items="warehouses" />} />
           <Route path="/warehouses/:warehouseId" element={<WarehouseDetails />} />
+
+          <Route path="/inventory" element={<ListItems items="inventory" />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
