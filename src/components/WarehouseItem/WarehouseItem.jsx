2de22f0acedc0715d02636a540642e4072a724c2
Tanya Vezyr <tetiana.vezyr@gmail.com>
2024-11-14 01:30:03 -0700

    fix styles for desktop

--- src/components/WarehouseItem/WarehouseItem.jsx
+++ src/components/WarehouseItem/WarehouseItem.jsx
@@ -1,7 +1,7 @@
 import "./WarehouseItem.scss";
-import ChevronIcon from "../../assets/Icons/chevron_right-24px.svg";
-import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg";
-import EditIcon from "../../assets/Icons/edit-24px.svg";
+import chevronIcon from "../../assets/Icons/chevron_right-24px.svg";
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
 
 import { Link } from "react-router-dom";
 
@@ -22,18 +22,12 @@ function WarehouseItem({ warehouse }) {
               </h3>
               <img
                 className="item__chevron-icon"
-                src={ChevronIcon}
+                src={chevronIcon}
                 alt="chevron-icon"
               />
             </Link>
           </div>
         </div>
-        <div className="item__info item__info--contact">
-          <h4 className="item__label">Contact Name</h4>
-          <p className="item__data p2">{warehouse.contact_name}</p>
-        </div>
-      </div>
-      <div className="item__row">
         <div className="item__info item__info--address">
           <h4 className="item__label">Address</h4>
           <p className="item__data p2">
@@ -41,6 +35,13 @@ function WarehouseItem({ warehouse }) {
             {warehouse.address}, {warehouse.city}, {warehouse.country}
           </p>
         </div>
+      </div>
+      <div className="item__row item__row--desktop">
+        <div className="item__info item__info--contact">
+          <h4 className="item__label">Contact Name</h4>
+          <p className="item__data p2">{warehouse.contact_name}</p>
+        </div>
+
         <div className="item__info item__info--contact">
           <h4 className="item__label">Contact Information:</h4>
           <p className="item__data p2">{warehouse.contact_phone}</p>
@@ -49,22 +50,21 @@ function WarehouseItem({ warehouse }) {
       </div>
       <div className="item__row item__row--actions">
         <div className="item__info item__info--delete">
-        <img
-          className="item__delete-icon"
-          src={DeleteIcon}
-          onClick={handleDeleteWarehouse}
-          alt="delete-icon"
-        />
+          <img
+            className="item__delete-icon"
+            src={deleteIcon}
+            onClick={handleDeleteWarehouse}
+            alt="delete-icon"
+          />
         </div>
         <div className="item__info item__info--edit">
-        <Link
-          className="item__edit-link"
-          to={`/warehouse/edit/${warehouse.id}`}
-        >
-          <img className="item__edit-icon" src={EditIcon} alt="edit icon" />
-        </Link>
+          <Link
+            className="item__edit-link"
+            to={`/warehouse/edit/${warehouse.id}`}
+          >
+            <img className="item__edit-icon" src={editIcon} alt="edit icon" />
+          </Link>
         </div>
-      
       </div>
     </div>
   );
