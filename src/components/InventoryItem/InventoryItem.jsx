d7b92a18f985b3e0f666f10a62a1fb0dee0a7d76
Tanya Vezyr <tetiana.vezyr@gmail.com>
2024-11-15 16:01:39 -0700

    implement InventoryDetails Component

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -63,7 +63,7 @@ function InventoryItem({ inventory, isFirst, isForWarehouseDetails, onDelete }) 
             <div className="inventory-item__right">
               <div className="inventory-item__container-status">
                 <h4 className="inventory-item__label">STATUS</h4>
-                <p className={`inventory-item__instock ${inventory.status === "Out of Stock" ? 'inventory-item__instock--false' : ''}`}>{inventory.status.toUpperCase()}</p>
+                <p className={`inventory-item__instock ${inventory.status === "Out of Stock" ? 'inventory-item__instock--false' : ''}`}>{inventory.status}</p>
               </div>
 
               <div className="inventory-item__container-quantity">
