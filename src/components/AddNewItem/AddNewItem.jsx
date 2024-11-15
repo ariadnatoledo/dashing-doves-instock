1167f4a9c9dbd41cd4196cd98dc7f9593e91ccb6
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-15 14:39:38 -0800

    add dropdown inputs and radio inputs

--- src/components/AddNewItem/AddNewItem.jsx
+++ src/components/AddNewItem/AddNewItem.jsx
@@ -1,44 +1,91 @@
 import "./AddNewItem.scss";
 import ComponentHeader from "../ComponentHeader/ComponentHeader";
 import SaveCancelAddButton from "../SaveCancelAddButton/SaveCancelAddButton";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function AddNewItem() {
+    const baseURL = import.meta.env.VITE_API_URL;
+
+    const [itemCategory, setitemCategory] = useState("");
+  
+
+
+
   return (
-    <form className='addNewItem-form'>
-                <ComponentHeader navigateTo="/inventory" text="Add New Item"   />
-                <section className='addNewItem'>
-                    <div className='itemDetails'>
+    <form className="addNewItem-form">
+      <ComponentHeader navigateTo="/inventory" text="Add New Item" />
+      <section className="addNewItem">
+        <div className="itemDetails">
+          <h2 className="itemDetails-title">Item Details</h2>
 
+          <label className="itemDetails-label">Item Name</label>
+          <textarea
+            className="itemDetails-input"
+            name="itemName"
+            placeholder="Item Name"
+            required
+          ></textarea>
 
-                        <h2 className='itemDetails-title'>Item Details</h2>
+          <label className="itemDetails-label"> Description </label>
+          <textarea
+            className="itemDetails-input itemDetails-input--description"
+            name="itemDescription"
+            placeholder="Please enter a brief item description..."
+            required
+          ></textarea>
 
-                        <label className='itemDetails-label'>Item Name</label>
-                        <textarea className='itemDetails-input' name='itemName' placeholder='Item Name' required></textarea>
+          <label className="itemDetails-label">Category</label>
+            <select className="itemDetails-input" name="itemCategory" id=""></select>
 
-                        <label className='itemDetails-label'> Description </label>
-                        <textarea className='itemDetails-input itemDetails-input--description' name='itemDescription' placeholder='Please enter a brief item description...' required></textarea>
+        </div>
+        <div className="border"></div>
+        <div className="itemAvailability">
+          <h2 className="itemAvailability-title"> Item Availability </h2>
 
-                        <label className='itemDetails-label'>Category</label>
-                        {/* <textarea className='itemDetails-city-input' name='warehouseCity' placeholder='Please Select' required></textarea> */}
+          <label className="itemAvailability-label">Status</label>
 
-                    </div>
-                    <div className='border'></div>
-                    <div className='itemAvailability'>
-                        <h2 className='itemAvailability-title'> Item Availability </h2>
-                        
-                        <label className='itemAvailability-label'>Quantity</label>
-                        <textarea className='itemAvailability-input' name='itemQuantity' placeholder='0' required></textarea>
+          <div className="itemAvailability__status">
+            <label htmlFor="stock" className="itemAvailability__radio-label">
+              <input
+                type="radio"
+                className="itemAvailability__radio"
+                name="stock"
+              />
+              In stock
+            </label>
 
-                        <label className='itemAvailability-label'>Warehouse</label>
-                        {/* <textarea className='itemAvailability-phone-input' name='contactPhone' placeholder='Please Select' required></textarea> */}
-                        
-                    </div>
-                </section>
+            <label htmlFor="stock" className="itemAvailability__radio-label">
+              <input
+                type="radio"
+                className="itemAvailability__radio"
+                name="stock"
+              />
+              Out of stock
+            </label>
+          </div>
 
+          <label className="itemAvailability-label">Quantity</label>
+          <textarea
+            className="itemAvailability-input"
+            name="itemQuantity"
+            placeholder="0"
+            required
+          ></textarea>
 
-                <SaveCancelAddButton showAddWarehouse={false} showAddItem={true} navigateTo={`/inventory`} />
-            </form>
-  )
+          <label className="itemAvailability-label">Warehouse</label>
+          <select className="itemAvailability-input" name="itemWarehouse" ></select>
+        </div>
+      </section>
+
+      <SaveCancelAddButton
+        showAddWarehouse={false}
+        showAddItem={true}
+        navigateTo={`/inventory`}
+      />
+    </form>
+  );
 }
 
-export default AddNewItem
+export default AddNewItem;
