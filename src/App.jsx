ffd24a9f73e1a523b739d08f991c63e790bd951c
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-15 11:41:57 -0800

    formatt

--- src/App.jsx
+++ src/App.jsx
@@ -13,37 +13,37 @@ import DeleteComponent from "./components/DeleteComponent/DeleteComponent.jsx";
 import AddNewItem from "./components/AddNewItem/AddNewItem.jsx";
 import EditItem from "./components/EditItem/EditItem.jsx";
 
-
-
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div className="main__card">
         <Routes>
-          
           <Route path="/" element={<ListItems items="warehouses" />} />
 
           <Route path="/warehouses" element={<Navigate to="/" />} />
-          <Route path="/warehouses/:warehouseId" element={<WarehouseDetails />} />
+          <Route
+            path="/warehouses/:warehouseId"
+            element={<WarehouseDetails />}
+          />
           <Route path="/warehouses/:id/edit" element={<EditWarehouse />} />
           <Route path="/warehouses/add-new" element={<AddNewWarehouse />} />
-           <Route path="/inventory" element={<ListItems items="inventories" />} />
-           <Route path="/inventory/add-new" element={<AddNewItem />} />
-           <Route path="/inventory/:id" element={<InventoryItem />} />
-           <Route path="/inventory/:id/edit" element={<EditItem />} />
-           <Route path="/inventory/:id/delete" element={<DeleteComponent/>}/>
-
+          <Route
+            path="/inventory"
+            element={<ListItems items="inventories" />}
+          />
+          <Route path="/inventory/add-new" element={<AddNewItem />} />
+          <Route path="/inventory/:itemId" element={<InventoryItem />} />
+          <Route path="/inventory/:itemId/edit" element={<EditItem />} />
+          <Route
+            path="/inventory/:itemId/delete"
+            element={<DeleteComponent />}
+          />
 
           <Route path="*" element={<NotFoundPage />} />
-
-
         </Routes>
       </div>
       <Footer />
-
-
     </BrowserRouter>
   );
 }
