d3f90282d36885265e5de6fe2f66e58e70d732cd
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-16 08:24:31 -0800

    add alert to cancel button

--- src/components/SaveCancelAddButton/SaveCancelAddButton.jsx
+++ src/components/SaveCancelAddButton/SaveCancelAddButton.jsx
@@ -1,34 +1,50 @@
-import React from 'react';
+import React from "react";
 import "./SaveCancelAddButton.scss";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-export default function SaveCancelAddButton({ showSave, showAddWarehouse, showAddItem, navigateTo }) {
+export default function SaveCancelAddButton({
+  showSave,
+  showAddWarehouse,
+  showAddItem,
+  navigateTo,
+}) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleCancelClick = () => {
+    const confirmCancel = confirm("Select OK to cancel form submission");
 
-    const handleCancelClick = () => {
-        navigate(navigateTo);
-    };
+    if (confirmCancel) {
+      navigate(navigateTo);
+    }
+  };
 
+  return (
+    <div className="saveCancel">
+      <button
+        type="button"
+        className="cancelButton"
+        onClick={handleCancelClick}
+      >
+        Cancel
+      </button>
 
+      {showSave && (
+        <button type="submit" className="saveButton">
+          Save
+        </button>
+      )}
 
-    return (
-        <div className='saveCancel'>
-            <button type="button" className="cancelButton" onClick={handleCancelClick}>Cancel</button>
+      {showAddWarehouse && (
+        <button type="submit" className="addWarehouse">
+          + Add Warehouse
+        </button>
+      )}
 
-            {showSave && (
-                <button type="submit" className="saveButton" >Save</button>
-            )}
-
-            {showAddWarehouse && (
-                <button type="submit" className="addWarehouse" >+ Add Warehouse</button>
-            )}
-
-            {showAddItem && (
-                <button type="submit" className="addItem" >+ Add Item</button>
-            )}
-
-
-        </div>
-    );
+      {showAddItem && (
+        <button type="submit" className="addItem">
+          + Add Item
+        </button>
+      )}
+    </div>
+  );
 }
